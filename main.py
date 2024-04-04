117d2bcfe99628d1f4f7c4d0393f7eaa0b14fc87
Jubiar01 <101883055+Jubiar01@users.noreply.github.com>
2024-04-04 17:14:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import logging
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, CallbackContext, CallbackQueryHandler
 from pb import decrypt_pb_file
@@ -7,9 +6,6 @@ from tnl import decrypt_tnl_file
 from sks import decrypt_sks_file
 from ziv import decrypt_ziv_file
 import os
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 ALLOWED_USERS_FILE = 'allowuser.txt'
 
@@ -28,8 +24,6 @@ async def start_help_handler(update: Update, context: CallbackContext):
     allowed_users = load_allowed_users()
     status = "allowed" if user_id in allowed_users else "not allowed"
     
-    logger.info(f"{user_name} - User ID: {user_id} - Command: /start - Status: {status}")
-
     keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Copy ID", callback_data=f"copy_id_{user_id}")]])
     help_message = (
         f"Hi {user_name}! Let's get you started:\n\n"
@@ -61,8 +55,6 @@ async def check_status(update: Update, context: CallbackContext):
     allowed_users = load_allowed_users()
     status = "allowed" if user_id in allowed_users else "not allowed"
     
-    logger.info(f"{user_name} - User ID: {user_id} - Command: /check_status - Status: {status}")
-
     status_message = f"Your user ID ({user_id}) is {status}."
     await context.bot.send_message(chat_id=update.message.chat_id, text=status_message)
 
@@ -70,8 +62,6 @@ async def document_handler(update: Update, context: CallbackContext):
     user = update.message.from_user
     user_id = str(user.id)
     user_name = f"{user.first_name} {user.last_name}" if user.last_name else user.first_name
-
-    logger.info(f"{user_name} - User ID: {user_id} - Command: Document upload")
 
     allowed_users = load_allowed_users()
     if user_id not in allowed_users:
@@ -85,7 +75,6 @@ async def document_handler(update: Update, context: CallbackContext):
         file_extension = file_name.split('.')[-1] if '.' in file_name else 'unknown'
 
         if file_extension not in ['nm', 'tnl', 'sks', 'ziv', 'pb']:
-            logger.info(f"{user_name} - Unsupported file type: {file_extension}")
             await context.bot.send_message(chat_id=update.message.chat_id, text=f"Sorry, {user_name}, unsupported file type: {file_extension}")
             return
 
@@ -104,10 +93,8 @@ async def document_handler(update: Update, context: CallbackContext):
 
         if decrypted_content:
             await context.bot.send_message(chat_id=update.message.chat_id, text=decrypted_content, reply_to_message_id=update.message.message_id)
-            logger.info(f"{user_name} - {file_extension} Decrypted content sent successfully.")
         else:
             await context.bot.send_message(chat_id=update.message.chat_id, text="Decryption failed.", reply_to_message_id=update.message.message_id)
-            logger.error(f"{user_name} - Failed to decrypt {file_extension} file.")
 
 async def button_callback(update: Update, context: CallbackContext):
     query = update.callback_query
@@ -115,7 +102,7 @@ async def button_callback(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=query.message.chat_id, text=f"Your User ID ({user_id}) has been copied successfully.")
 
 def main():
-    token = "6100263145:AAHp0wupqPLVPCJxx4p8qlmgPo8aqbkH4gc"
+    token = "6100263145:AAHp0wupqPLVPCJxx4p8qlmgPo8aqbkH4gc"  # Replace YOUR_BOT_TOKEN with your actual bot token
     app = ApplicationBuilder().token(token).build()
     
     app.add_handler(CommandHandler("start", start_help_handler))
@@ -123,8 +110,6 @@ def main():
     app.add_handler(CommandHandler("check_status", check_status))
     app.add_handler(MessageHandler(filters.Document.ALL, document_handler))
     app.add_handler(CallbackQueryHandler(button_callback, pattern=r"^copy_id_\d+$"))
-
-    logger.info("Bot is running now!")
 
     app.run_polling()
 
